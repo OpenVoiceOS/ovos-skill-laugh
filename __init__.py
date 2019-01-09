b3797f992e4ee85296f8be04f784c67e8fa716d0
jarbasal <jarbasai@mailfence.com>
2019-01-09 14:55:54 +0000

    pep8 and stuff

--- __init__.py
+++ __init__.py
@@ -3,11 +3,23 @@ skill laugh
 
 Copyright (C) 2018 JarbasAI
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
 
 """
 
@@ -44,16 +56,17 @@ class LaughSkill(MycroftSkill):
     def initialize(self):
         sounds_dir = join(self.settings["sounds_dir"], "male")
         self.sounds["male"] = [join(sounds_dir, sound) for sound in
-                       listdir(sounds_dir) if ".wav" in sound or ".mp3" in
-                       sound]
+                               listdir(sounds_dir) if
+                               ".wav" in sound or ".mp3" in
+                               sound]
         sounds_dir = join(self.settings["sounds_dir"], "female")
         self.sounds["female"] = [join(sounds_dir, sound) for sound in
-                               listdir(sounds_dir) if
-                               ".wav" in sound or ".mp3" in sound]
-        sounds_dir = join(self.settings["sounds_dir"], "robot")
-        self.sounds["robot"] = [join(sounds_dir, sound) for sound in
                                  listdir(sounds_dir) if
                                  ".wav" in sound or ".mp3" in sound]
+        sounds_dir = join(self.settings["sounds_dir"], "robot")
+        self.sounds["robot"] = [join(sounds_dir, sound) for sound in
+                                listdir(sounds_dir) if
+                                ".wav" in sound or ".mp3" in sound]
         # stop laughs for speech execution
         self.add_event("speak", self.stop_laugh)
 
@@ -81,7 +94,8 @@ class LaughSkill(MycroftSkill):
         self.random_laugh = True
         self.handle_laugh_event(message)
 
-    @intent_handler(IntentBuilder('StopLaughing').require('Stop').require('Laugh'))
+    @intent_handler(
+        IntentBuilder('StopLaughing').require('Stop').require('Laugh'))
     def halt_laughing(self, message):
         self.log.info("Laughing skill: Stopping")
         # if in random laugh mode, cancel the scheduled event
@@ -124,4 +138,3 @@ class LaughSkill(MycroftSkill):
 
 def create_skill():
     return LaughSkill()
-
