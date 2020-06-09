1a5c5522d5d31f7d568d662ffc8638ce41a27972
jarbasal <jarbasai@mailfence.com>
2020-06-09 01:10:55 +0100

    big screen

--- __init__.py
+++ __init__.py
@@ -1,28 +1,3 @@
-"""
-skill laugh
-
-Copyright (C) 2018 JarbasAI
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-IN THE SOFTWARE.
-
-"""
-
 from mycroft import MycroftSkill, intent_file_handler, intent_handler
 from adapt.intent import IntentBuilder
 from mycroft.audio import wait_while_speaking, is_speaking
@@ -38,6 +13,8 @@ class LaughSkill(MycroftSkill):
         MycroftSkill.__init__(self)
         self.random_laugh = False
         self.sounds = {"male": [], "female": []}
+        if "haunted" not in self.settings:
+            self.settings["haunted"] = True
         if "gender" not in self.settings:
             self.settings["gender"] = "male"
         if "sounds_dir" not in self.settings:
@@ -70,18 +47,41 @@ class LaughSkill(MycroftSkill):
         # stop laughs for speech execution
         self.add_event("speak", self.stop_laugh)
 
+        self.add_event('skill-laugh.jarbasskills.home',
+                       self.homepage)
+
+        if self.settings["haunted"]:
+            self.random_laugh = True
+            self.handle_laugh_event(None)
+
+    def homepage(self):
+        caption = "A skill by Jarbas AI"
+        if self.settings["haunted"]:
+            caption = "Your hardware is haunted"
+        self.gui.clear()
+        self.gui.show_image(join(dirname(__file__), "ui", "images",
+                                 "jurassic_jarbas.png"),
+                            caption=caption,
+                            fill='PreserveAspectFit')
+
     def laugh(self):
         # dont laugh over a speech message
         if is_speaking():
             wait_while_speaking()
 
         sound = random.choice(self.sounds[self.settings["gender"]])
+
+        self.gui.clear()
+        pic = random.randint(0, 3)
+        self.gui.show_image(join(dirname(__file__), "ui", "images",
+                                 str(pic) + ".jpg"))
         if ".mp3" in sound:
-            self.p = play_mp3(sound)
+            self.p = play_mp3(sound).wait()
         elif ".ogg" in sound:
-            self.p = play_ogg(sound)
+            self.p = play_ogg(sound).wait()
         else:
-            self.p = play_wav(sound)
+            self.p = play_wav(sound).wait()
+        self.gui.clear()
 
     @intent_file_handler("Laugh.intent")
     def handle_laugh_intent(self, message):
@@ -104,6 +104,7 @@ class LaughSkill(MycroftSkill):
             self.random_laugh = False
             self.cancel_scheduled_event('random_laugh')
             self.speak_dialog("cancel")
+            # if haunted == True it will be back on reboot ;)
         else:
             self.speak_dialog("cancel_fail")
 
@@ -133,6 +134,8 @@ class LaughSkill(MycroftSkill):
         if self.random_laugh:
             self.halt_laughing(None)
             stopped = True
+        if stopped:
+            self.gui.clear()
         return stopped
 
 
